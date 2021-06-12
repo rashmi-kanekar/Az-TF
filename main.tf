7b8ba7010f5a3a732cc0bea2d216e5ad08b21e8e
Rashmi S Kanekar <Rashmi.S.Kanekar@ey.com>
2021-06-12 16:45:17 +0530

    Point backend to correct TFC org and workspace

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "remote" {
+    organization = "RSKTest2021"
+
+    workspaces {
+      name = "github-actions-test1"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm"{
+    features{}
+}
+
+resource "azurerm_resource_group" "rg"{
+    name     = "myTFResourceGroup"
+    location = "WestEurope"
+    tags = {
+        Environment = "Terraform Getting Started"
+        Team = "DevOps"
+    }
+}
